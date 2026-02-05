956fe5b8b41d61b666c02460d698af5c36d0e832
vishrutavasava-cyber <vishrutavasava@gmail.com>
2026-02-05 19:58:40 +0530

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "anime-tracker-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
